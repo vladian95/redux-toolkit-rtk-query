0538094a24cc3a6c059de727fd36d4bbc0a42beb
vladian95 <kvankvan1995@gmail.com>
2023-05-21 15:58:29 +0300

    added recipe item

---
+++ src/recipe-item/RecipeItem.jsx
@@ -0,0 +1,11 @@
+const RecipeItem = ({ recipe }) => {
+  return (
+    <div className="card">
+      {/* <img src="" alt="img" /> */}
+      <h2>{recipe.name}asdasdasd</h2>
+      <button>Add to favorites</button>
+    </div>
+  );
+};
+
+export default RecipeItem;
---
+++ src/store/favorites/favorites.slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+export const favoritesSlices = createSlice({
+  name: 'favorites',
+  initialState,
+  reducers: () => {
+    toggleFavorites: (state, { payload: recipe }) => {
+      const isExists = state.some((r) => r.id === recipe.id);
+      if (isExists) {
+        state = state.filter((r) => r.id !== recipe.id);
+      } else {
+        state.push(recipe);
+      }
+    };
+  },
+});
+
+export const { actions, reducers } = favoritesSlices;
